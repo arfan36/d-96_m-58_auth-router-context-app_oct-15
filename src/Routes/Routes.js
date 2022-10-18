d47c6521cc1740df868ceb2bb2e11594ebe69fd7
arfan36 <arfanur36@gmail.com>
2022-10-18 07:29:35 +0600

    58-1 Module Introduction and Basic Project Setup

---
+++ src/Routes/Routes.js
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home";
+import Login from "../components/Login";
+import Register from "../components/Register";
+import Main from "../layout/Main";
+
+export const Router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
+        ]
+    }
+]);
